c07a4df8d1d941438818636d732f814aa0c6fd13
Aydar Farrakhov <aydar.farravkho@akvelon.com>
2018-05-11 17:40:20 +0300

    fix modal

--- imports/ui/components/Error/index.js
+++ imports/ui/components/Error/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Modal, Typography } from 'material-ui';
+import { Modal, Typography, withStyles } from 'material-ui';
 
-const Error = ({ error = null, onClose }) => (
+const Error = ({ error = null, onClose, classes }) => (
   <Modal
     aria-labelledby="simple-modal-title"
     aria-describedby="simple-modal-description"
     open={error !== null && error.length > 0}
     onClose={onClose}
   >
-    <div>
+    <div className={classes.paper}>
       <Typography variant="title">
           Error
       </Typography>
@@ -20,10 +20,25 @@ const Error = ({ error = null, onClose }) => (
   </Modal>
 );
 
+
+const styles = theme => ({
+  paper: {
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    position: 'absolute',
+    width: theme.spacing.unit * 50,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing.unit * 4,
+  },
+});
+
 Error.propTypes = {
   error: PropTypes.string,
+  classes: PropTypes.object.isRequired,
   onClose: PropTypes.func.isRequired,
 };
 
-export default Error;
+export default withStyles(styles)(Error);
 
